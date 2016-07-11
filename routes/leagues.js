2da37e250a407cc2adf7cf78cd7d32c28cdfb46a
John Carney <jcarney000@gmail.com>
2016-07-11 14:10:11 -0700

    Added leaderboard get route.

--- routes/leagues.js
+++ routes/leagues.js
@@ -1,9 +1,11 @@
 'use strict';
 
-// todo: leaderboard display
+/* eslint-disable camelcase */
 
 const knex = require('../knex');
 const express = require('express');
+
+// eslint-disable-next-line new-cap
 const router = express.Router();
 const ev = require('express-validation');
 const validations = require('../validations/leagues');
@@ -17,8 +19,8 @@ const checkAuth = function(req, res, next) {
   next();
 };
 
-router.post('/league', ev(validations.postLeague), (req, res, next) =>{
-  const {name, password} = req.body;
+router.post('/league', ev(validations.postLeague), (req, res, next) => {
+  const { name, password } = req.body;
 
   knex('leagues')
     .select(knex.raw('1=1'))
@@ -27,22 +29,23 @@ router.post('/league', ev(validations.postLeague), (req, res, next) =>{
     .then((nameRes) => {
       if (nameRes) {
         const err = new Error('League name already exists.');
+
         err.status(400);
 
         throw err;
       }
 
-      return bcrypt.hash(password, 10)
+      return bcrypt.hash(password, 10);
     })
-    .then((hashedPass) => {
-      return knex('leagues')
+    .then((hashedPass) =>
+      knex('leagues')
         .insert({
-          name: name,
+          name,
           hashed_password: hashedPass
         }, '*')
-    })
-    .then((user) => {
-      res.sendStatus(200)
+    )
+    .then(() => {
+      res.sendStatus(200);
     })
     .catch((err) => {
       next(err);
@@ -51,7 +54,14 @@ router.post('/league', ev(validations.postLeague), (req, res, next) =>{
 
 router.post('/league/player', ev(validations.postPlayer), (req, res, next) => {
   let leagueId;
-  const { name, leaguePassword, firstName, lastName, playerEmail, playerPassword  } = req.body;
+  const {
+    name,
+    leaguePassword,
+    firstName,
+    lastName,
+    playerEmail,
+    playerPassword
+  } = req.body;
 
   knex('players')
     .select(knex.raw('1=1'))
@@ -62,14 +72,16 @@ router.post('/league/player', ev(validations.postPlayer), (req, res, next) => {
         return res
           .status(400)
           .set('Content-Type', 'text/plain')
-          .send('Email already exists.')
+          .send('Email already exists.');
       }
+
       return knex('leagues')
         .where('name', name)
         .first()
         .then((leagueRes) => {
           if (!leagueRes) {
             const err = new Error('Invalid league name or password.');
+
             err.status = 401;
 
             throw err;
@@ -78,11 +90,9 @@ router.post('/league/player', ev(validations.postPlayer), (req, res, next) => {
 
           return bcrypt.compare(leaguePassword, leagueRes.hashed_password);
         })
-        .then(() => {
-          return bcrypt.hash(playerPassword, 10);
-        })
-        .then((hashedPass) => {
-          return knex('players')
+        .then(() => bcrypt.hash(playerPassword, 10))
+        .then((hashedPass) =>
+          knex('players')
             .insert({
               first_name: firstName,
               last_name: lastName,
@@ -90,27 +100,35 @@ router.post('/league/player', ev(validations.postPlayer), (req, res, next) => {
               hashed_password: hashedPass,
               league_id: leagueId
             }, '*')
-        })
-        .then((user) => {
+        )
+        .then(() => {
           res.sendStatus(200);
         })
         .catch(bcrypt.MISMATCH_ERROR, () => {
-          const err = new Error('Invalid league name or password.')
+          const err = new Error('Invalid league name or password.');
+
           err.status = 401;
 
           throw err;
-        })
+        });
     })
     .catch((err) => {
-      console.log(err);
       next(err);
     });
 });
 
-router.post('/league/game', ev(validations.postGame), (req, res, next) => {
-  const { team1P1Id, team1P2Id, team2P1Id, team2P2Id, team1Score, team2Score } = req.body;
+// eslint-disable-next-line max-len
+router.post('/league/game', checkAuth, ev(validations.postGame), (req, res, next) => {
+  const {
+    team1P1Id,
+    team1P2Id,
+    team2P1Id,
+    team2P2Id,
+    team1Score,
+    team2Score
+  } = req.body;
 
-  let newGame = {
+  const newGame = {
     team1_p1_id: team1P1Id,
     team2_p1_id: team2P1Id,
     team1_score: team1Score,
@@ -125,8 +143,45 @@ router.post('/league/game', ev(validations.postGame), (req, res, next) => {
 
   knex('games')
     .insert(newGame, '*')
-    .then((game) => {
-      res.sendStatus(200)
+    .then(() => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/league/games', checkAuth, (req, res, next) => {
+  const leagueId = req.session.leagueId;
+  const leaderboard = {};
+
+  knex('players')
+    .select('first_name', 'last_name', 'elo')
+    .where('league_id', leagueId)
+    .orderBy('elo', 'desc')
+    .then((rankings) => {
+      leaderboard.rankings = rankings;
+      const subquery = knex('games')
+        .distinct('games.id')
+        .orderBy('games.id', 'desc')
+        .limit(2);
+
+      return knex('games')
+        .select(
+          'games.id',
+          'first_name',
+          'last_name',
+          'team1_score',
+          'team2_score'
+        )
+        .innerJoin('players', 'players.league_id', 'games.league_id')
+        .where('games.league_id', leagueId)
+        .whereIn('games.id', subquery)
+        .orderBy('games.id', 'desc');
+    })
+    .then((recentGames) => {
+      leaderboard.recentGames = recentGames;
+      res.status(200).send(leaderboard);
     })
     .catch((err) => {
       next(err);
